cb5915d084a750494cfa64c10c42f28b0786cba2
lonsoien <loubelons@gmail.com>
2023-09-11 14:52:30 +0200

    link portfolio removed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -66,7 +66,7 @@ function About() {
             </div>
             <div className="container-animation">
                 <div className="select-button">
-                    <a href="https://louisebellingmo-portfolio.netlify.app/work">PORTFOLIO</a>
+                    <a href="#">PORTFOLIO</a>
                 </div>
             </div>
             <br/>
